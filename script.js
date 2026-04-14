c94c8a6d7fe42e202978c68d3d842dce7c702883
Ehotdog <elilichtenstein13@gmail.com>
2026-04-14 16:14:11 -0400

    Add Firebase configuration and initialization
    Initialize Firebase with configuration settings.

--- script.js
+++ script.js
@@ -20,3 +20,15 @@ function postStory() {
   document.getElementById("title").value = "";
   document.getElementById("content").value = "";
 }
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBb59VjFAhgE2D6zM5O07m32MQR83N16Kk",
+  authDomain: "lives-saved-through-shar-536f8.firebaseapp.com",
+  projectId: "lives-saved-through-shar-536f8",
+  storageBucket: "lives-saved-through-shar-536f8.firebasestorage.app",
+  messagingSenderId: "898935254971",
+  appId: "1:898935254971:web:57d7c4c0819a7aca931e9e"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
